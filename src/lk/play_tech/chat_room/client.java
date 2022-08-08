44d6471d21c2416c7ffc5db304549de1f792bed6
sajithmadusamkha <sajithmadusankha02@gmail.com>
2022-08-08 13:36:47 +0530

    client class added

---
+++ src/lk/play_tech/chat_room/client.java
@@ -0,0 +1,25 @@
+package lk.play_tech.chat_room;
+
+import java.io.*;
+import java.net.Socket;
+
+public class client {
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String userName;
+
+    public client(Socket socket, String userName) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.userName = userName;
+        } catch (IOException e) {
+            close_everything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    private void close_everything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+    }
+}
