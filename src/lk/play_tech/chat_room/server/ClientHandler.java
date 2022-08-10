7d9a6416986b8dcedfea61f6ec8d4f6053dd8d5e
sajithmadusamkha <sajithmadusankha02@gmail.com>
2022-08-10 11:05:39 +0530

    server & clientHandler

--- src/lk/play_tech/chat_room/server/ClientHandler.java
+++ src/lk/play_tech/chat_room/server/ClientHandler.java
@@ -1,5 +1,51 @@
 package lk.play_tech.chat_room.server;
 
-public class ClientHandler{
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler extends Thread{
+    private ArrayList<ClientHandler> clientHandlers;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private PrintWriter printWriter;
+
+    public ClientHandler(Socket socket , ArrayList<ClientHandler> clientHandlers) {
+        try{
+            this.socket = socket;
+            this.clientHandlers = clientHandlers;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.printWriter = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        try{
+            String massage;
+            while ((massage = bufferedReader.readLine()) != null) {
+                if(massage.equalsIgnoreCase("exit")) {
+                    break;
+                }
+
+                for(ClientHandler c : clientHandlers) {
+                    c.printWriter.println(massage);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        finally {
+            try {
+                bufferedReader.close();
+                printWriter.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
--- src/lk/play_tech/chat_room/server/Server.java
+++ src/lk/play_tech/chat_room/server/Server.java
@@ -1,4 +1,24 @@
 package lk.play_tech.chat_room.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Server {
+    private static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5000);
+        Socket accept;
+
+        while(true) {
+            System.out.println("Waiting for client!");
+            accept = serverSocket.accept();
+            System.out.println("A new client has connected!");
+            ClientHandler clientHandler = new ClientHandler(accept,clientHandlers);
+            clientHandlers.add(clientHandler);
+            clientHandler.start();
+        }
+    }
 }
